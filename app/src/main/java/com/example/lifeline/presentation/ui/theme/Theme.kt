893cd37a78f2b45cb6bccf13f518edf0d823be2e
jojoclt <chalanthorn_jojo@hotmail.com>
2022-05-27 13:09:35 +0800

    bump (just forced only whitetheme)

--- app/src/main/java/com/example/lifeline/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/example/lifeline/presentation/ui/theme/Theme.kt
@@ -2,12 +2,10 @@ package com.example.lifeline.presentation.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Red
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -42,7 +40,8 @@ private val LightColorPalette = lightColors(
 @Composable
 fun LifelineTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
-        DarkColorPalette
+        LightColorPalette
+//        DarkColorPalette
     } else {
         LightColorPalette
     }
