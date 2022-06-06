480934dcfcdb8bd9b9f59fad9c0004ca230f801d
Song <porama2001.11@gmail.com>
2022-06-07 04:12:14 +0800

    Make List, Today more complete, Timer must use realtime not recompose

---
+++ app/src/main/java/com/example/lifeline/data/local/SampleList.kt
@@ -0,0 +1,18 @@
+package com.example.lifeline.data.local
+
+
+data class SampleTask(
+    val detail: String
+)
+object SampleList {
+
+    val simpletasklist = listOf(
+        SampleTask("Buy Milk"),
+        SampleTask("Buy Peanut"),
+        SampleTask("Do laundry"),
+        SampleTask("Finish Software studio project"),
+        SampleTask("Look at professor profile"),
+        SampleTask("Wake KK and Audrey up"),
+        SampleTask( "LOL")
+    )
+}
--- app/src/main/java/com/example/lifeline/presentation/today/composables/TodayScreen.kt
+++ app/src/main/java/com/example/lifeline/presentation/today/composables/TodayScreen.kt
@@ -5,26 +5,32 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.lifeline.R
+import com.example.lifeline.data.local.SampleList
+import com.example.lifeline.data.local.SampleTask
 import com.example.lifeline.presentation.BottomNav
 import com.example.lifeline.presentation.TopNav
 import com.example.lifeline.presentation.ui.theme.LifelineTheme
 import com.example.lifeline.util.Screen
+import kotlinx.coroutines.delay
 
 
 @Composable
@@ -41,18 +47,88 @@ fun TodayScreen(navController: NavController) {
             Text(
                 text = "Estimated Time to Completed"
             )
+            TimeRemain()
             CupCanvas()
             Row() {
                 Text(text = "To dos") //later make it be on left
                 Spacer(modifier = Modifier.width(8.dp))
                 Text(text = "See More") //later make it be on right
             }
-            LazyColumn(
-
+            Surface(
+                shape = MaterialTheme.shapes.large,
+                elevation = 3.dp,
+                color = Color.LightGray,
             ) {
-                item { Text(text = "wait for data") }
+            TaskList(SampleList.simpletasklist)
             }
         }
+    }
+}
+
+@Composable
+fun TaskList(tasks: List<SampleTask>){
+    LazyColumn{
+        items(tasks){task->TaskCard(task)}
+    }
+}
+
+@Composable
+fun TaskCard(t: SampleTask){
+    Surface(
+        shape = MaterialTheme.shapes.medium,
+        elevation = 1.dp,
+    ) {
+        Text( text = t.detail)
+    }
+}
+
+@Composable
+fun TimeRemain(){
+    var remainingHour by remember {
+        mutableStateOf(24L)
+    }
+    var remainingMin by remember {
+        mutableStateOf(0L)
+    }
+    LaunchedEffect(key1 = remainingHour, key2 = remainingMin){
+        if(remainingMin >= 0) {
+            delay(200L)
+            remainingMin -= 1L
+            //value = remainingHour/TotalHour.toFloat()
+        }
+        else{
+            remainingMin = 59L
+            remainingHour -= 1L
+        }
+        if(remainingHour <= 0){
+            remainingHour = 24L
+        }
+    }
+    Row(){
+        Text(
+            text = remainingHour.toString(),
+            fontSize = 44.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Red
+        )
+        Text(
+            text = "hr",
+            fontSize = 44.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Red
+        )
+        Text(
+            text = remainingMin.toString(),
+            fontSize = 44.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Red
+        )
+        Text(
+            text = "min",
+            fontSize = 44.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Red
+        )
     }
 }
 
