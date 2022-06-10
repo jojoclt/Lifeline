7622c317153983ca43ec1dfd42c64bfe56da3d78
jojoclt <chalanthorn_jojo@hotmail.com>
2022-06-11 01:38:52 +0800

    Canvas in progress

--- app/src/main/java/com/example/lifeline/presentation/today/composables/CanvasCup.kt
+++ app/src/main/java/com/example/lifeline/presentation/today/composables/CanvasCup.kt
@@ -6,17 +6,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.scale
 import androidx.compose.ui.graphics.drawscope.translate
+import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.lifeline.R
 import com.example.lifeline.util.VectorHelper
 
+// https://developer.android.com/reference/android/graphics/BlendMode.html#SRC
+// https://stackoverflow.com/questions/65653560/jetpack-compose-applying-porterduffmode-to-image BUG
 @Composable
+//@Preview
 fun CupCanvas() {
     val context = LocalContext.current
 
@@ -38,15 +42,23 @@ fun CupCanvas() {
                 color = Color.LightGray,
                 size = Size(size.width,size.height*0.3f), topLeft = Offset(x = 0f, y = 0.7f*size.height)
             )
-            with(mask) {
-                scale(0.7f, Offset(size.width/1.96f,size.height/1.8f), block = {draw(size)})
-            }
+
             with(cup) {
                 translate(left = size.width/18f ,block = { draw(size) })
             }
-
+        }
+        Canvas(modifier = Modifier.fillMaxSize()) {
+            with(drawContext.canvas.nativeCanvas) {
+                val checkPoint = saveLayer(null, null)
+                with(mask) {
+                    scale(0.7f, Offset(size.width/1.96f,size.height/1.8f), block = {draw(size)})
+                }
+                drawRect(color = Color.Yellow, topLeft = Offset(110f,100f), size = Size(size.width*0.5f,size.height*0.5f),blendMode = BlendMode.SrcIn)
+                restoreToCount(checkPoint)
+            }
 
         }
+
     }
 }
 
@@ -62,16 +74,5 @@ fun DrawLine() {
             color = Color.Blue,
             strokeWidth = 16.0f
         )
-    }
-}
-
-@Preview
-@Composable
-fun CupCanvasPreview() {
-    Box(
-        Modifier
-            .fillMaxWidth()
-            .aspectRatio(1f)) {
-        CupCanvas()
     }
 }
