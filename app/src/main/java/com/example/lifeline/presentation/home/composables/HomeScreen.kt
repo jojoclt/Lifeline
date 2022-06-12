970c169e55ae28d9a1e72697b874dd53dad83dff
jojoclt <chalanthorn_jojo@hotmail.com>
2022-06-13 01:41:24 +0800

    Merge with Audrey

--- app/src/main/java/com/example/lifeline/presentation/home/composables/HomeScreen.kt
+++ app/src/main/java/com/example/lifeline/presentation/home/composables/HomeScreen.kt
@@ -5,23 +5,25 @@ import android.util.Log
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.material.Card
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.inset
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.graphics.vector.VectorPainter
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -37,7 +39,6 @@ import com.example.lifeline.presentation.TopNav
 import com.example.lifeline.presentation.today.TodayViewModel
 import com.example.lifeline.presentation.ui.theme.*
 import com.example.lifeline.util.Screen
-import dagger.hilt.android.lifecycle.HiltViewModel
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -51,7 +52,7 @@ fun HomeScreen(navController: NavController, viewModel: TodayViewModel = hiltVie
     /**
      * weather[0] denotes the previous, now and next
      */
-    val magicBoolean = viewModel.getTasks().isEmpty()
+    val magicBoolean = viewModel.getTodoTask().isEmpty()
 
     Scaffold(
         topBar = { TopNav(currentScreen = currentScreen) },
